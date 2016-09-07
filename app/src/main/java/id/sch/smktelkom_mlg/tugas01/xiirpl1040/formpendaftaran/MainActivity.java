947d103cb1605af69bc854de7f43fb2b1ddb7c49
Widyarso Joko Purnomo <widyjp10@gmail.com>
2016-09-07 10:31:18 +0700

    membuat fungsi button proses, untuk memproses hasil inputan dan ditampilkan pada textview

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1040/formpendaftaran/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1040/formpendaftaran/MainActivity.java
@@ -2,14 +2,17 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl1040.formpendaftaran;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +26,14 @@ public class MainActivity extends AppCompatActivity {
     CheckBox offense, defense, special, setuju;
     Button proses, daftar;
     TextView hasil_nama, hasil_kelas, hasil_ttl, hasil_umur, hasil_SB, hasil_tim, hasil_alasan;
+    LinearLayout llHasil;
     List<String> list_kelas;
     ArrayAdapter<String> spinnerAdapter;
+    private int year;
+    private String _sb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -51,7 +57,109 @@ public class MainActivity extends AppCompatActivity {
         hasil_SB = (TextView) findViewById(R.id.tvHasilSB);
         hasil_tim = (TextView) findViewById(R.id.tvHasilTim);
         hasil_alasan = (TextView) findViewById(R.id.tvHasilAlasan);
+        llHasil = (LinearLayout) findViewById(R.id.llHasil);
         setSpinnerEntries();
+
+        proses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (nama.getText().toString().equals("")
+                        || tgl_lahir.getText().toString().equals("")
+                        || bulan_lahir.getText().toString().equals("")
+                        || tahun_lahir.getText().toString().equals("")
+                        || kelas.getSelectedItem().toString().equals("")
+                        || SB.getCheckedRadioButtonId() == -1
+                        || alasan.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Masih ada field yang belum diisi", Toast.LENGTH_SHORT).show();
+                } else if (Integer.parseInt(tgl_lahir.getText().toString()) < 1 || Integer.parseInt(tgl_lahir.getText().toString()) > 31) {
+                    tgl_lahir.setError("Format tanggal salah");
+                } else if (Integer.parseInt(bulan_lahir.getText().toString()) < 1 || Integer.parseInt(bulan_lahir.getText().toString()) > 12) {
+                    bulan_lahir.setError("Format bulan salah");
+                } else if (!offense.isChecked()
+                        && !defense.isChecked()
+                        && !special.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "Anda belum memilih tim", Toast.LENGTH_SHORT).show();
+                } else {
+                    llHasil.setVisibility(View.VISIBLE);
+                    llHasil.requestFocus();
+                    hasil_nama.setText("Nama     : " + nama.getText().toString());
+                    hasil_kelas.setText("Kelas    : " + kelas.getSelectedItem().toString());
+                    hasil_ttl.setText("Lahir    : " + tgl_lahir.getText().toString() + " - " + getMonth(bulan_lahir.getText().toString()) + " - " + tahun_lahir.getText().toString());
+                    hasil_umur.setText("Umur     : " + getYear(tahun_lahir.getText().toString()));
+                    hasil_tim.setText("Tim      : " + setTim());
+                    hasil_alasan.setText("Alasan   : " + alasan.getText().toString());
+                    hasil_SB.setText(getSB() + " tahu olahraga Flag Football sebelumnya");
+                }
+            }
+        });
+    }
+
+    public String getSB() {
+        RadioButton rb = (RadioButton) findViewById(SB.getCheckedRadioButtonId());
+        _sb = rb.getText().toString();
+        return _sb;
+    }
+
+    private String setTim() {
+        String tim = "";
+        if (offense.isChecked()) {
+            tim += "Offense, ";
+        }
+        if (defense.isChecked()) {
+            tim += "Defense, ";
+        }
+        if (special.isChecked()) {
+            tim += "Special.";
+        }
+        return tim;
+    }
+
+    private String getMonth(String bulan) {
+        String month = "";
+        switch (bulan) {
+            case "1":
+                month = "Januari";
+                break;
+            case "2":
+                month = "Februari";
+                break;
+            case "3":
+                month = "Maret";
+                break;
+            case "4":
+                month = "April";
+                break;
+            case "5":
+                month = "Mei";
+                break;
+            case "6":
+                month = "Juni";
+                break;
+            case "7":
+                month = "Juli";
+                break;
+            case "8":
+                month = "Agustus";
+                break;
+            case "9":
+                month = "September";
+                break;
+            case "10":
+                month = "Oktober";
+                break;
+            case "11":
+                month = "November";
+                break;
+            case "12":
+                month = "Desember";
+                break;
+        }
+        return month;
+    }
+
+    public String getYear(String tahun) {
+        year = 2016 - Integer.parseInt(tahun);
+        return String.valueOf(year);
     }
 
     private void setSpinnerEntries() {
